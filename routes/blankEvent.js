9b76e4a6493152fdb8869ae77ad2b19afc7f063b
Elisa Zhang <e7zhang@ucsd.edu>
2021-02-23 16:45:08 -0800

    blankEvent

--- routes/blankEvent.js
+++ routes/blankEvent.js
@@ -1,3 +1,19 @@
+var events = require('../views/events.json');
+
 exports.view = function(req, res){
-    res.render('blankEvent');
+	var name = req.params.eventName;
+	var eventType = req.params.eventType;
+	var petType = req.params.petType;
+	var breed = req.params.breed;
+	var time = req.params.time;
+	var location = req.params.location;
+
+    res.render('blankEvent', {
+    	'eventName': name,
+    	'eventType': eventType,
+    	'petType': petType,
+    	'breed': breed,
+    	'time': time,
+    	'location': location
+    });
 };
